9310430de317ba15d9478f29202f5fb198b6db6a
Dylan King <dwkblue51@yahoo.com>
2023-01-05 10:52:01 -0600

    handle sign up

---
+++ controllers/api-controller.js
@@ -0,0 +1,46 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const {body, validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+// handle new user registration
+exports.signUpPost = [
+    body('username', 'Username must not be empty')
+    .trim()
+    .isLength({min: 1})
+    .escape(),
+    body('password', 'Password must not be empty')
+    .trim()
+    .isLength({min: 1})
+    .escape(),
+    body('firstName').escape(),
+    body('lastName').escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        bcrypt.hash(req.body.password, 8, (err, hash) => {
+            const user = new User({
+              username: req.body.username,
+              password: hash,
+              firstName: req.body.firstName ?? null,
+              lastName: req.body.lastName ?? null,
+              isAdmin: false,
+            });
+            if (err) {
+              return next(err);
+            }
+            if (!errors.isEmpty()) {
+              return res
+                .status(400)
+                .json({ message: "error registering user", user: user });
+            }
+            // successful
+            user.save((err) => {
+              if (err) {
+                return next(err);
+              }
+            });
+            return res.status(200).json({ user });
+          });
+    }
+]
