4b32abaaed0ea4bb433d9f59b45d2b3bc63748f9
Dylan King <dylan.wesley.king@gmail.com>
2023-01-07 21:13:24 -0600

    add axios, redis packages. add recipes ref to UserSchema

--- models/user.js
+++ models/user.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-const findOrCreate = require('mongoose-findorcreate');
+const mongoose = require("mongoose");
+const findOrCreate = require("mongoose-findorcreate");
 
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    firstName: String,
-    lastName: String,
-
+  firstName: String,
+  lastName: String,
+  recipes: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
 });
 
 UserSchema.plugin(findOrCreate);
